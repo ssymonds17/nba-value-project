7ae5a221cf11efeae294ca3746adf906788d286f
Sam Symonds <samuelsymonds2017@gmail.com>
2020-10-07 13:31:11 +0100

    Updated cors policy with new corsOptions variable and set to *

--- index.js
+++ index.js
@@ -9,25 +9,10 @@ const Players = Models.Player;
 
 app.use(bodyParser.json());
 
-// (WIP) setting sites that have access 
-let allowedOrigins = '*';
-// ['https://nba-value-reference.herokuapp.com/', 'http://localhost:3000/', 'http://localhost:1234'];
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
-
-// Connection with MongoDB. CONNECTION_URI variable established in Heroku to protect database security
-// mongoose.connect(process.env.CONNECTION_URI, {
-//  useNewUrlParser: true,
-//  useUnifiedTopology: true
-// });
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
 
 // FOR LOCAL USE, DELETE BEFORE PRODUCTION
 mongoose.connect('mongodb+srv://ssymonds17:klrGPn2TwoQKpxi5@cluster0.bwlxj.mongodb.net/nbadata?retryWrites=true&w=majority', {
@@ -36,7 +21,7 @@ mongoose.connect('mongodb+srv://ssymonds17:klrGPn2TwoQKpxi5@cluster0.bwlxj.mongo
 });
 
 // GET player data by player ID
-app.get('/players/:playerID', (req, res) => {
+app.get('/players/:playerID', cors(corsOptions), (req, res) => {
   Players.find({ player_id: req.params.playerID })
     .then((player) => {
       res.status(200).json(player);
@@ -56,3 +41,25 @@ const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
   console.log('Listening on Port ' + port);
 });
+
+// ORIGINAL CORS POLICY
+// (WIP) setting sites that have access 
+// let allowedOrigins = '*';
+// // ['https://nba-value-reference.herokuapp.com/', 'http://localhost:3000/', 'http://localhost:1234'];
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+
+// ENVIRONMENTAL VARIABLE HOSTED IN HEROKU
+// Connection with MongoDB. CONNECTION_URI variable established in Heroku to protect database security
+// mongoose.connect(process.env.CONNECTION_URI, {
+//  useNewUrlParser: true,
+//  useUnifiedTopology: true
+// });
