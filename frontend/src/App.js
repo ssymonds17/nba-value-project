dbc6278bedd08d298904b48eaf7dc0516b69b248
Sam Symonds <samuelsymonds2017@gmail.com>
2020-10-07 13:14:22 +0100

    Set up parcel, babel and react devdependencies. Updated cors in index.js for all domains

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      player: []
+    };
+  }
+
+  componentDidMount() {
+    let playerID = window.location.pathname;
+    console.log(playerID);
+    this.getPlayerData(playerID);
+  }
+
+  getPlayerData = (playerID) => {
+    axios.get(`https://nba-value-reference.herokuapp.com${playerID}`)
+      .then((response) => {
+        this.setState({
+          player: response.data
+        });
+      })
+      .catch(() => {
+        alert('data has not been received');
+      })
+  }
+
+  render() {
+    const { player } = this.state;
+    return (
+      <div>
+        {player.map(player => (
+          <p>{player.name}</p>
+        ))}
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn how to React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- index.js
+++ index.js
@@ -10,47 +10,48 @@ const Players = Models.Player;
 app.use(bodyParser.json());
 
 // (WIP) setting sites that have access 
-let allowedOrigins = '*';
+let allowedOrigins = ['https://nba-value-reference.herokuapp.com/', 'http://localhost:3000/', 'http://localhost:1234'];
 app.use(cors({
- origin: (origin, callback) => {
-  if (!origin) return callback(null, true);
-  if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
-   let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
-   return callback(new Error(message), false);
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn't found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
   }
-  return callback(null, true);
- }
 }));
 
-// (WIP) Need to update Connection URI with Heroku
-mongoose.connect(process.env.CONNECTION_URI, {
- useNewUrlParser: true,
- useUnifiedTopology: true
-});
-
-// mongoose.connect('mongodb+srv://ssymonds17:klrGPn2TwoQKpxi5@cluster0.bwlxj.mongodb.net/nbadata?retryWrites=true&w=majority', {
+// Connection with MongoDB. CONNECTION_URI variable established in Heroku to protect database security
+// mongoose.connect(process.env.CONNECTION_URI, {
 //  useNewUrlParser: true,
 //  useUnifiedTopology: true
 // });
 
+// FOR LOCAL USE, DELETE BEFORE PRODUCTION
+mongoose.connect('mongodb+srv://ssymonds17:klrGPn2TwoQKpxi5@cluster0.bwlxj.mongodb.net/nbadata?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 // GET player data by player ID
 app.get('/players/:playerID', (req, res) => {
- Players.find({ player_id: req.params.playerID })
-  .then((player) => {
-   res.status(200).json(player);
-  })
-  .catch((e) => {
-   console.error(e);
-   res.status(500).send('Error ' + e);
-  });
+  Players.find({ player_id: req.params.playerID })
+    .then((player) => {
+      res.status(200).json(player);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).send('Error ' + e);
+    });
 });
 
 app.use(express.static('frontend/build'));
 app.get('/*', function (req, res) {
- res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => {
- console.log('Listening on Port ' + port);
+  console.log('Listening on Port ' + port);
 });
