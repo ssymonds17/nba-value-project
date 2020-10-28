840612e8a07e274abaceaba8773e017458ad022a
Sam Symonds <samuelsymonds2017@gmail.com>
2020-10-28 14:12:43 +0000

    Separated active and defunct franchises on team-list-view by creating two new functions to sort the original franchiseList

--- frontend/src/components/team-list-view.js
+++ frontend/src/components/team-list-view.js
@@ -5,34 +5,83 @@ import { Link } from 'react-router-dom';
 
 export class TeamListView extends React.Component {
 
+   getDefunct = () => {
+      const defunctFranchises = [];
+      franchiseList.forEach(function (element, index) {
+         if (element['active'] === 'no') {
+            defunctFranchises.push(element);
+         }
+      });
+      return defunctFranchises;
+   }
+
+   getActive = () => {
+      const activeFranchises = [];
+      franchiseList.forEach(function (element, index) {
+         if (element['active'] === 'yes') {
+            activeFranchises.push(element);
+         }
+      });
+      return activeFranchises;
+   }
+
    render() {
+      let defunctFranchises = this.getDefunct();
+      let activeFranchises = this.getActive();
+
       return (
-         <table>
-            <thead>
-               <tr className="team-list-header">
-                  <td>Team Name</td>
-                  <td>League</td>
-                  <td>From</td>
-                  <td>Until</td>
-               </tr>
-            </thead>
-            <tbody>
-               {
-                  franchiseList.map(
-                     (team) => (
-                        <tr className="team-list-body" key={team.abbreviation}>
-                           <td><Link to={`/teams/${team.abbreviation}`}>{team.team_full_name}</Link></td>
-                           <td>{team.league}</td>
-                           <td>{team.first_year}</td>
-                           <td>{team.last_year}</td>
-                           {
-                              team.active === "no" ?
-                                 <td>Defunct</td> : ''
-                           }
-                        </tr>
-                     ))}
-            </tbody>
-         </table>
+         <div className="franchise-list-container">
+            <div className="active-franchises">
+               <h2>Active Franchises</h2>
+               <table>
+                  <thead>
+                     <tr className="team-list-header">
+                        <td>Team Name</td>
+                        <td>League</td>
+                        <td>From</td>
+                        <td>Until</td>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {
+                        activeFranchises.map(
+                           (team) => (
+                              <tr className="team-list-body" key={team.abbreviation}>
+                                 <td><Link to={`/teams/${team.abbreviation}`}>{team.team_full_name}</Link></td>
+                                 <td>{team.league}</td>
+                                 <td>{team.first_year}</td>
+                                 <td>{team.last_year}</td>
+                              </tr>
+                           ))}
+                  </tbody>
+               </table>
+            </div>
+            <div className="defunct-franchises">
+               <h2>Defunct Franchises</h2>
+               <table>
+                  <thead>
+                     <tr className="team-list-header">
+                        <td>Team Name</td>
+                        <td>League</td>
+                        <td>From</td>
+                        <td>Until</td>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     {
+                        defunctFranchises.map(
+                           (team) => (
+                              <tr className="team-list-body" key={team.abbreviation}>
+                                 <td><Link to={`/teams/${team.abbreviation}`}>{team.team_full_name}</Link></td>
+                                 <td>{team.league}</td>
+                                 <td>{team.first_year}</td>
+                                 <td>{team.last_year}</td>
+                              </tr>
+                           ))}
+                  </tbody>
+               </table>
+            </div>
+         </div>
       )
    }
 }
